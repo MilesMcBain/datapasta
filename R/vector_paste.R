69d73f432076c6f179ffde4643a293b2378371bd
MilesMcBain <miles.mcbain@gmail.com>
2016-11-14 15:37:55 +1000

    Added vertical version of vector_paste()

--- R/vector_paste.R
+++ R/vector_paste.R
@@ -6,24 +6,9 @@
 #' @export
 #'
 vector_paste <- function(){
-  clipboard_string <- clipr::read_clip()
-  nspc <- .rs.readUiPref('num_spaces_for_tab')
-  context <- rstudioapi::getActiveDocumentContext()
-  context_row <- context$selection[[1]]$range$end["row"]
-  indent_context <- nchar(context$contents[context_row])
 
-  if(length(clipboard_string == 1)){
-      clipboard_vector <- unlist(
-        strsplit(
-          x = clipboard_string,
-          split = "\t|,|\\|",
-          perl= TRUE)
-        )
-  }else{
-      clipboard_vector <- clipboard_string
-  }
-   
-  vector_type <- readr::guess_parser(clipboard_vector)
+  clipboard_vector <- parse_vector()
+  vector_type <- attr(clipboard_vector, "type")
 
   vector_form <- paste0("c(",
     paste0(
@@ -33,6 +18,44 @@ vector_paste <- function(){
     ")"
   )
   rstudioapi::insertText(vector_form)
+}
+
+vector_paste_vertical <- function(){
+  clipboard_vector <- parse_vector()
+  vector_type <- attr(clipboard_vector, "type")
+  
+  nspc <- .rs.readUiPref('num_spaces_for_tab')
+  context <- rstudioapi::getActiveDocumentContext()
+  context_row <- context$selection[[1]]$range$end["row"]
+  indent_context <- nchar(context$contents[context_row])
+  
+  vector_form <- paste0("c(",
+                    paste0(
+                      #ifelse(is.na(clipboard_vector), yes = "NA", no = paste0('"',clipboard_vector,'"'))
+                      lapply(clipboard_vector, render_type, vector_type), 
+                      collapse = paste0(",\n",strrep(" ", indent_context + nspc))
+                    ),
+                    ")"
+                  )     
+  rstudioapi::insertText(vector_form)
+}
+
+parse_vector <- function(){
+  clipboard_string <- clipr::read_clip()
+  if(length(clipboard_string == 1)){
+    clipboard_vector <- unlist(
+      strsplit(
+        x = clipboard_string,
+        split = "\t|,|\\|",
+        perl= TRUE)
+    )
+  }else{
+    clipboard_vector <- clipboard_string
+  }
+  vector_type <- readr::guess_parser(clipboard_vector)
+  
+  attr(clipboard_vector, "type") <- vector_type
+  clipboard_vector
 }
 
 render_type <- function(char_vec, type_str){
