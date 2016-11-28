d9de37b518c8174a12dda31c1d27b4860f488e97
Miles McBain <miles.mcbain@gmail.com>
2016-11-28 20:41:41 +1000

    Use clipr::clipr_available() to skip tests.
    Also remove readme.html

--- tests/testthat/test_vector.R
+++ tests/testthat/test_vector.R
@@ -1,7 +1,9 @@
 context("test vector paste")
+skip_msg <- "System clipboard is not available - skipping test."
+is_clipr_available <- clipr::clipr_available()
 
 test_that("A vector of strings is parsed correctly", {
-    skip_on_cran()
+    skip_if_not(is_clipr_available, skip_msg)
     expect_equal({
                 clipr::write_clip(content = c("Normal",
                                                 "Steel",
@@ -27,7 +29,7 @@ test_that("A vector of strings is parsed correctly", {
 })
 
 test_that("A vector of numbers is parsed correctly", {
-    skip_on_cran()
+    skip_if_not(is_clipr_available, skip_msg)
     expect_equal({
         suppressWarnings(clipr::write_clip(content = c(7,
                                                       8,
@@ -54,7 +56,7 @@ test_that("A vector of numbers is parsed correctly", {
 })
 
 test_that("Vector_paste formats numeric correctly ", {
-  skip_on_cran()
+  skip_if_not(is_clipr_available, skip_msg)
   expect_equal({
     suppressWarnings(clipr::write_clip(content = c(7,
                                                    8,
@@ -73,7 +75,7 @@ test_that("Vector_paste formats numeric correctly ", {
 
 
 test_that("Vector_paste formats combine numeric/strings correctly ", {
-    skip_on_cran()
+    skip_if_not(is_clipr_available, skip_msg)
     expect_equal({
         suppressWarnings(clipr::write_clip(content = c("a","6", "b", "4"))
         )
@@ -85,7 +87,7 @@ test_that("Vector_paste formats combine numeric/strings correctly ", {
 
 
 test_that("Vector_paste handles empty strings", {
-    skip_on_cran()
+    skip_if_not(is_clipr_available, skip_msg)
     expect_equal({
     suppressWarnings(clipr::write_clip(content = c("a","6", "", "4"))
     )
