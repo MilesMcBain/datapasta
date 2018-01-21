d27294a6b31d4570d941e2818e8d5411f8bef1b0
milesmcbain <miles.mcbain@gmail.com>
2018-01-21 10:37:57 +1000

    Parsing of horizontal vectors for dfiddle working

--- R/dfiddle.R
+++ R/dfiddle.R
@@ -6,17 +6,21 @@ dfiddle <- function(){
 
   if(is_naked_vec(doc_context)){
     regular_delimited <-
-      paste0(split_naked_vector(doc_context)[[1]], collapse=", ")
+      paste0(split_naked_vector(doc_context), collapse=", ")
     vector_form <- paste0("c(",regular_delimited,")")
   } # parse to a vector
 
-  is_horiz_vector # pivot to vertical
+  if(is_horiz_vec(doc_context)){
+    vec_elems <-
+      paste0(split_horiz_vec(doc_context))
 
-  is_vert_vector # pivot to quoted horiz vector
+  } # pivot to vertical
 
-  is_quoted_horiz_vector # pivot to quoted vertical vector
+  is_vert_vec # pivot to quoted horiz vector
 
-  is_quoted_vert_vector # pivot to unqoted horizontal
+  is_quoted_horiz_vec # pivot to quoted vertical vector
+
+  is_quoted_vert_vec # pivot to unqoted horizontal
 
   selection_result <-
     tryCatch( expr = eval(parse(text = doc_context$text), envir = eval_env),
@@ -30,11 +34,25 @@ dfiddle <- function(){
 
 is_naked_vec <- function(text){
  text <- trimws(text)
- naked_vector <- "^(((\"|\')[^\'\"]+(\"\')|\\w+)(\\s)*(,)*(\n)*(\\s)*)+"
+ naked_vector <- "^(((\"|\')[^\'\"]+(\"|\')|\\w+)(\\s)*(,)*(\n)*(\\s)*)+"
  grep(x = text, pattern = naked_vector) == 1
 }
 
 split_naked_vector<- function(naked_vec){
   strsplit(x = naked_vec, split = "(?<=(\"|'|\\w))[\\s,\n]+",
-           perl = TRUE)
+           perl = TRUE)[[1]]
+}
+
+is_horiz_vec <- function(text){
+  text <- trimws(text)
+  horiz_vector <- "c\\(((\\s)*((\"|\')[^\'\"]+(\"|\')|\\w+)(\\s*)(,)*)+\\)"
+  grep(x = text, pattern = horiz_vector) == 1
+}
+
+split_horiz_vec <- function(horiz_vec){
+  horiz_elems <- gsub( pattern = "(^c\\(|\\)$)",
+                       replacement = "",
+                       x = trimws(horiz_vec))
+  strsplit(x = horiz_elems, split = "(?<=(\"|'|\\w))[\\s,]+",
+    perl = TRUE)[[1]]
 }
