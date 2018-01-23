1c6c35a15a5378b6ae25a648fd626a4b0edf5681
milesmcbain <miles.mcbain@gmail.com>
2018-01-23 22:38:24 +1000

    Toggle quote working

--- R/dfiddle.R
+++ R/dfiddle.R
@@ -2,6 +2,7 @@ zzz_rs_dfiddle <- function(){
   if(!is_rstudio_selection()) return(invisible(NULL))
 
   # If we have a data.frame or tibble, repaste (or replace) to get a neatly formatted output.
+
   doc_context <- rstudioapi::primary_selection(rstudioapi::getActiveDocumentContext())
   indent_context <- attr(regexpr(pattern = "^\\s*(?=\\S)", text = doc_context$text, perl = TRUE), "match.length")
   eval_env <- new.env()
@@ -42,7 +43,7 @@ zzz_rs_dfiddle <- function(){
       paste0(strrep(" ", indent_context), "c(",
         paste0(
           split_horiz_vec(doc_context$text),
-          collapse = paste0(",\n",strrep(" ", indent_context + (doc_context$range$start[2]-1) + 2)) #2 to align for 'c('
+          collapse = paste0(",\n",strrep(" ", indent_context + (doc_context$range$start[2]-1) + 2)) # 2 to align for 'c('
         ),
         ")"
       )
@@ -67,6 +68,54 @@ zzz_rs_dfiddle <- function(){
   return(invisible(vector_form))
 
 }
+
+zzz_rs_toggle_quotes <- function(){
+  if(!is_rstudio_selection()) return(invisible(NULL))
+  doc_context <- rstudioapi::primary_selection(rstudioapi::getActiveDocumentContext())
+  indent_context <- attr(regexpr(pattern = "^\\s*(?=\\S)", text = doc_context$text, perl = TRUE), "match.length")
+  vector_form <- NA
+
+  if(is_vert_vec(doc_context$text)){
+    content <- toggle_quote_elems( split_vert_vec(doc_context$text) )
+
+    vector_form <-
+      paste0(strrep(" ", indent_context), "c(",
+             paste0(
+               content,
+               collapse = paste0(",\n",strrep(" ", indent_context + (doc_context$range$start[2]-1) + 2)) # 2 to align for 'c('
+             ),
+             ")"
+      )
+    end_of_range <- last_line_content_length(vector_form) + indent_context + 1
+    insert_range <-
+      rstudioapi::document_range(doc_context$range$start,
+                                 c(doc_context$range$start[1] + n_lines(vector_form)-1, end_of_range)
+      )
+
+  }
+
+  if(is_horiz_vec(doc_context$text)){
+    content <- toggle_quote_elems( split_horiz_vec(doc_context$text) )
+
+    vector_form <- paste0(strrep(" ", indent_context),"c(",
+                          paste0(content , collapse = ","),
+                          ")")
+    insert_range <- rstudioapi::document_range(doc_context$range$start,
+                                               c(doc_context$range$start[2], nchar(vector_form)+1)
+    )
+
+  }
+  if(!is.na(vector_form)){
+    rstudioapi::modifyRange(doc_context$range, text = vector_form)
+    rstudioapi::setSelectionRanges(insert_range)
+    return(invisible(vector_form))
+  }
+
+}
+
+#########################################################################
+# Utilities for dfiddle addins
+########################################################################
 
 is_naked_vec <- function(text){
  text <- trimws(text)
@@ -113,16 +162,57 @@ split_vert_vec <- function(vert_vec){
 }
 
 n_lines <- function(a_structure){
-  length(gregexpr(pattern = "\n", text = a_structure)[[1]])+1 #The last line won't have a /n on it so +1.
+  length(gregexpr(pattern = "\n", text = a_structure)[[1]])+1 # The last line won't have a /n on it so +1.
 }
 
 last_line_content_length <- function(a_structure){
   attr(gregexpr(pattern = "(?<=\n).*$", text = a_structure, perl = TRUE)[[1]], "match.length")
 }
 
-zzz_rs_toggle_quotes <- function(){
-  if(!is_rstudio_selection()) return(invisible(NULL))
-  doc_context <- rstudioapi::primary_selection(rstudioapi::getActiveDocumentContext())
-  indent_context <- attr(regexpr(pattern = "^\\s*(?=\\S)", text = doc_context$text, perl = TRUE), "match.length")
-
+toggle_quote_elems <- function(content){
+  quoted_content <- vapply(content, is_elem_quoted, logical(1))
+  # If not all quoted, then quote all
+  # If all quotes unqoute all
+  if( !(all(quoted_content)) ){
+    content[!quoted_content] <- vapply(content[!quoted_content], quote_elem, character(1))
+  } else {
+    content <- vapply(content, unquote_elem, character(1))
+  }
+  content
 }
+
+is_elem_quoted <- function(chr_vec){
+  if(length(chr_vec) != 1) stop("is_elem_quoted called on char vec of length > 1")
+  matches <- grep(pattern = "^[\"\'].*[\"\']$", x = chr_vec, perl = TRUE)
+  length(matches) > 0 && matches == 1
+}
+
+quote_elem <- function(chr_vec){
+  if(length(chr_vec) != 1) stop("quote_elem called on char vec of length > 1")
+  paste0('"',chr_vec,'"')
+}
+
+unquote_elem <- function(chr_vec){
+  if(length(chr_vec) != 1) stop("unquote_elem called on char vec of length > 1")
+  gsub(pattern ="(^[\'\"])|([\'|\"]$)", replacement = "", x = chr_vec, perl = TRUE)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
