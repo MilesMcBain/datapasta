64624747b682679cda3403b0bdee228d44646845
Miles McBain <miles.mcbain@gmail.com>
2019-07-10 23:30:42 +1000

    df/dt paste refactor working for most cases
    commma separated character inputs are no longer line wrapped inconveniently

--- R/dfdt_paste.R
+++ R/dfdt_paste.R
@@ -114,48 +114,47 @@ dfdt_construct <- function(input_table, oc = console_context(), class = NULL) {
   charw <- max(max(nchar(col_names_valid)) + 3L, 12L)
 
   #Generate lists of data ready for formatting
-  list_of_cols <- lapply(which(col_types != "factor"), function(x) list(name = paste0(pad_to(col_names_valid[x], charw),
-                                                                        call = "c("),
+  list_of_cols <- lapply(which(col_types != "factor"), function(x) list(name = paste0(pad_to(col_names_valid[x], charw)),
+                                                                        call = "= c(",
                                                                         data = unlist(lapply(cols[[x]], render_type, col_types[[x]])),
-                                                                        close = ")")
-                   )
+                                                                        close = ")"))
+
   #Handle the factor columns specially.
   if(any(col_types == "factor")){
     list_of_factor_cols <-
       lapply(which(col_types == "factor"), function(x) list(name = pad_to(col_names_valid[x], charw),
-                                                            call = "as.factor(c(",
+                                                            call = "= as.factor(c(",
                                                             data = unlist(lapply(cols[[x]], render_type, col_types[[x]])),
-                                                            close = "))")
-      )
+                                                            close = "))"))
 
     list_of_cols <- c(list_of_cols, list_of_factor_cols)
     names(list_of_cols) <- names(cols)
   }
 
-  #paste0 inserts its own "\n" for lists which will mess witch what we're trying to do with tortellini.
-  #For now, just stripping "\n" from the output.
-  list_of_cols <- lapply(list_of_cols, function(X) gsub("\n", "", X))
-
   output <- paste0(
-    paste0(paste0(ifelse(oc$indent_head, yes = strrep(" ", oc$indent_context), no = ""),
+    paste0(paste0(ifelse(oc$indent_head,
+                         yes = strrep(" ", oc$indent_context),
+                         no = ""),
+                  ifelse(class == "data.frame",
+                         yes = "data.frame(",
+                         no = "data.table::data.table("),
 
-                  ifelse(class == "data.frame", "data.frame(", "data.table::data.table("),
-
-                  ifelse(contains_chars && class == "data.frame", yes = "stringsAsFactors=FALSE,", no=""), "\n",
+                  ifelse(contains_chars && class == "data.frame",
+                         yes = "stringsAsFactors=FALSE,\n",
+                         no=""),
 
                   ifelse(any(col_names_valid != names(cols)),
-                         yes = tortellini("check.names=FALSE",
-                                          indent_context = nchar("data.frame("),
-                                          add_comma = TRUE), no = "")),
-
+                         yes = "check.names=FALSE,\n",
+                         no = "")
+                  ),
            ifelse(exists("row_names"),
-                  yes = tortellini(paste0("row.names = c(",
-                                          paste0(paste0("'", row_names, "'"), collapse = ", "),
-                                          ")"),
+                  yes = tortellini(list(name = "row.names",
+                                        call = "= c(",
+                                        data = row_names,
+                                        close =  ")"),
                                    indent_context = oc$indent_context,
                                    add_comma = TRUE),
                   no = ""),
-
            paste0(sapply(list_of_cols[1:(length(list_of_cols) - 1)],
                          function(x) tortellini(x, indent_context = oc$indent_context, add_comma = TRUE)), collapse = ""),
            paste0(sapply(list_of_cols[length(list_of_cols)],
@@ -163,53 +162,64 @@ dfdt_construct <- function(input_table, oc = console_context(), class = NULL) {
            strrep(" ", oc$indent_context),")\n"
     ), collapse = "")
 
-
   return(invisible(output))
 }
 
 #' wrap the datapasta around itself
-#' @param s input string
-#' @param n number of characters for text (includes column name on line 1)
+#' @param col_struct input structure - a split apart column definition
+#' @param defn_width total number of characters in a line (includes column name and indent on line 1)
 #' @param indent_context the level of indent in spaces in the current editor pane
 #' @param add_comma add one final comma to the end of the wrapped column def? Useful when pasting together columns.
 #' @return w wrapped string
 
-tortellini <- function(s, n = 80, indent_context = 0, add_comma = TRUE) {
+tortellini <- function(col_struct, defn_width = 80, indent_context = 0, add_comma = TRUE) {
+
+  split_s <- list(paste(col_struct$name, col_struct$call),
+         col_struct$data,
+         col_struct$close)
+
+  joined_s <- paste0(paste0(split_s[[1]],
+                            paste0(split_s[[2]], collapse = ", ")),
+                     split_s[[3]])
+
+  ## calculate indent context
 
   ## if the string is less than n chars then
   ## don't worry about splitting
-  if (nchar(s) > n) {
+  if ((nchar(joined_s) + indent_context) > defn_width) {
 
   ## determine the initial offset
-  offset <- attr(regexpr("^.*?c\\(", s), "match.length") - 1L + indent_context
+  offset <- nchar(split_s[[1]]) + indent_context
 
-  ## split the string at commas
-  split_s <- strsplit(s, ",")[[1]]
+  ## try to fit the whole defn within n chars, but draw a line at data with of 20
+  ## so we have something to work with
+  group_length <- max(defn_width - offset, 20)
 
   ## determine the groups of strings by splitting at n chars
-  ## additional 1 is for the comma to be added back
-  groups <- cumsum(nchar(split_s) + 1L) %/% n
+  ## additional 2 is for the comma space to be added back
+  groups <- (indent_context + nchar(split_s[[1]]) + cumsum(nchar(split_s[[2]]) + 2L)) %/% group_length
 
-  ## paste the first group of strings back together
-  wrapped_s <- paste0(strrep(" ", indent_context),
-                      paste0(split_s[groups == groups[1]], collapse = ",")
-               )
+  ## paste groups together
+  wrapped_data <- sapply(unique(groups),
+                      function(x) paste0(
+                        strrep(" ", indent_context),
+                        paste0(split_s[[2]][groups == x], collapse = ",")))
 
-  ## for the remaining groups, subtract the offset
-  ## from the width limit then re-calculate groupings
-  split_s_rem <- split_s[groups != groups[1]]
-  groups_rem <- cumsum(nchar(split_s_rem) + 1L) %/% (n - offset)
-  ngroups <- length(unique(groups_rem))
+  ## for group 1 add the definition call
+  ## needs whitespace trimmed because it had indent_context added which needs to be
+  ## added at front of line
+  wrapped_data[1] <- paste0(strrep(" ", indent_context), split_s[[1]], trimws(wrapped_data[1]))
 
-  ## paste the remaining groups together
-  wrapped_s[2:(ngroups+1)] <- sapply(unique(groups_rem),
-                                     function(x) paste0(
-                                       strrep(" ", offset),
-                                       paste0(split_s_rem[groups_rem == x], collapse = ",")))
+  ## for groups after group 1, add the offset
+  wrapped_data[2:length(wrapped_data)] <- paste0(strrep(" ", offset), wrapped_data[2:length(wrapped_data)])
+
+  ## join parts together for final output
+  wrapped_s <- paste0(paste0(wrapped_data, collapse = ",\n"),
+         split_s[3])
 
   } else { ## if no splitting is required
 
-    wrapped_s <- paste0(strrep(" ", indent_context), s)
+    wrapped_s <- joined_s
 
   }
 
